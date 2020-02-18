87c64944c467b54a41d6ef49133bd6bcd220a030
Agatha North <rouborn@gmail.com>
2020-02-17 22:59:18 -0800

    functional commit

---
+++ details.js
@@ -0,0 +1,51 @@
+'use strict'
+//Quiz App
+
+/* renderShoppingList()
+Requirements
+1: The function must iterate through store items
+2: Generate new <li> DOM elements for each item
+3: Place or replace the contents of <ul> with new <il> elements
+
+1: Does NOT rely on any input from the user or the DOM. It only reads
+from the store.
+2: Does NOT make any changes to the store. -read only-
+3: Does NOt worry about how a <li> is generated -- another function should
+be responsible for that template
+*/ 
+
+/*Quizapp Questions and Answers
+
+Welcome to our AD&D 2nd Edition Quiz!
+
+1: What does THAC0 stand for?
+    a. To Hit Armor Class 0
+    b. The Highest Armor Class, 0
+    c. The Holy Angel Council 0
+    d. The History A Chronicle: 0
+
+2: The spell Fireball has which of the following characteristics
+    a. Arcane, Conjuration, 1st level, 2d4/CL 20d4 max
+    b. Divine, Elemental Fire, 4th level, 3d8+1/CL 3d8+20 max
+    c. Arcane, Evocation, 3rd level, 1d6/CL 10d6 max
+    d. Arcane, Incantation, 3rd level, 1d6/CL 15d6 max
+
+3: Which of the following is a real Sage/Specialist class?
+    a. Mystic
+    b. Cartographer
+    c. Warlock
+    d. Songmage
+
+4: At what level does a Cleric achieve their Lord Level?
+    a. 10th
+    b. 11th
+    c. 8th
+    d. 9th
+
+5: Which of these is not a real breed of Dragons?
+    a. Mercury Dragon
+    b. Cloud Dragon
+    c. Amethyst Dragon
+    d. Ice Dragon
+
+*/
